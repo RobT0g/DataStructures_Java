34ae8b4e6ea320ea5b7e95a257bd11a6bdaf7f69
Robson Luan <64618248+RobT0g@users.noreply.github.com>
2022-06-07 21:55:29 -0300

    Update Main.java

--- ListaEncadeada/src/main/java/com/mycompany/Class07_06/Main.java
+++ ListaEncadeada/src/main/java/com/mycompany/Class07_06/Main.java
@@ -10,6 +10,20 @@ package com.mycompany.Class07_06;
  */
 public class Main {
     public static void main(String [] args){
-        System.out.println("Hello world!");
+        Lista list = new Lista();
+        list.insertAt(10, 1);
+        list.push(2);
+        list.insert(1);
+        list.insert(2);
+        list.insert(3);
+        list.insert(4);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+        //4 3 2 1 10 2 2 3 4
+        list.insertAt(10, 5);
+        list.insertAt(12, 4);
+        list.insertAt(13, 3);
+        System.out.println(list);
     }
 }
