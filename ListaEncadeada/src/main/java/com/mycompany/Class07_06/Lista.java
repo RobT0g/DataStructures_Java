db0f22692cc2d95ee3cb1535b04adf7bfc0e3ccd
Robson Luan <64618248+RobT0g@users.noreply.github.com>
2022-06-07 21:28:56 -0300

    Create Lista.java

---
+++ ListaEncadeada/src/main/java/com/mycompany/Class07_06/Lista.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.Class07_06;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Celula {
+    private Celula prox;
+    private Object elem;
+    
+    public Celula(Object elem, Celula prox){
+        this.prox = prox;
+        this.elem = elem;
+    }
+    
+    public Celula(Object elem){
+        this.prox = prox;
+        this.elem = elem;
+    }
+
+    public Celula getProx() {
+        return prox;
+    }
+
+    public void setProx(Celula prox) {
+        this.prox = prox;
+    }
+
+    public Object getElem() {
+        return elem;
+    }
+
+    public void setElem(Object elem) {
+        this.elem = elem;
+    }
+}
